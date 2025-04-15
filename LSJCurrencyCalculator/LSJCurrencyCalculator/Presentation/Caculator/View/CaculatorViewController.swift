032785d880ec4a5c30919c8b4f1d137f0bc8281c
Yimkeul <leesjun29@gmail.com>
2025-04-15 21:08:37 +0900

    [Feat] #4 - CaculatorView UI 구성

--- LSJCurrencyCalculator/LSJCurrencyCalculator/Presentation/Caculator/View/CaculatorViewController.swift
+++ LSJCurrencyCalculator/LSJCurrencyCalculator/Presentation/Caculator/View/CaculatorViewController.swift
@@ -14,6 +14,43 @@ final class CaculatorViewController: UIViewController {
     
     private let viewModel: CaculatorViewModel
     
+    private let labelStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 4
+        $0.alignment = .center
+    }
+    
+    private let currencyLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 24, weight: .bold)
+    }
+    
+    private let countryLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .gray
+    }
+    
+    private let amountTextField = UITextField().then {
+        $0.borderStyle = .roundedRect
+        $0.keyboardType = .decimalPad
+        $0.textAlignment = .center
+        $0.placeholder = "금액을 입력하세요"
+    }
+    
+    private let convertButton = UIButton().then {
+        $0.backgroundColor = .systemBlue
+        $0.layer.cornerRadius = 8
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        $0.setTitle("환율 계산", for: .normal)
+    }
+    
+    private let resultLabel = UILabel().then {
+        $0.text = "계산 결과가 여기에 표시됩니다"
+        $0.font = .systemFont(ofSize: 20, weight: .medium)
+        $0.textAlignment = .center
+        $0.numberOfLines = 0
+    }
+    
     init(viewModel: CaculatorViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -27,9 +64,44 @@ final class CaculatorViewController: UIViewController {
         super.viewDidLoad()
         navigationItem.title = "환율 계산기"
         setStyles()
+        setupLayout()
+        configure()
     }
     
     private func setStyles() {
         view.backgroundColor = .systemBackground
+    }
+    
+    private func setupLayout() {
+        labelStackView.addArrangedSubviews(currencyLabel, countryLabel)
+        view.addSubviews(labelStackView, amountTextField, convertButton, resultLabel)
+        
+        labelStackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(32)
+            make.centerX.equalToSuperview()
+        }
+        amountTextField.snp.makeConstraints { make in
+            make.top.equalTo(labelStackView.snp.bottom).offset(32)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.height.equalTo(44)
+        }
+        
+        convertButton.snp.makeConstraints { make in
+            make.top.equalTo(amountTextField.snp.bottom).offset(24)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.height.equalTo(44)
+        }
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalTo(convertButton.snp.bottom).offset(32)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+        }
+    }
+    
+    func configure() {
+        currencyLabel.text = "TEST"
+        countryLabel.text = "테스트"
     }
 }
