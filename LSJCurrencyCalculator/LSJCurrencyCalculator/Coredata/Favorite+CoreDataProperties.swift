7d492a3cf198e52f24ab8fe0941af403470d2a3a
Yimkeul <leesjun29@gmail.com>
2025-04-16 15:34:14 +0900

    [Comment] #7 - CoreData 주석 추가

--- LSJCurrencyCalculator/LSJCurrencyCalculator/Coredata/Favorite+CoreDataProperties.swift
+++ LSJCurrencyCalculator/LSJCurrencyCalculator/Coredata/Favorite+CoreDataProperties.swift
@@ -26,3 +26,10 @@ extension Favorite {
 extension Favorite : Identifiable {
 
 }
+
+/*
+- `@nonobjc` = Objective-C 에서는 동작하지 않고 Swift 에서만 동작하는 메서드임을 명시.
+- `fetchRequest()` = Favorite 에 대한 여러가지 데이터 검색을 도움.
+- `@NSManaged` = CoreData 에 의해 관리되는 객체를 의미.
+- `Identifiable` = Favorite 타입이 고유하게 식별될 수 있음을 의미.
+*/
