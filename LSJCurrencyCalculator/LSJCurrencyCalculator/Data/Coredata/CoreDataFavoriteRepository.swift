68f9f4f6a7309d8407f511c66ae6a6c7aa3f0839
Yimkeul <leesjun29@gmail.com>
2025-04-16 16:15:35 +0900

    [Feat] #7 - Data Layer에 Coredata추가

--- LSJCurrencyCalculator/LSJCurrencyCalculator/Data/Coredata/CoreDataFavoriteRepository.swift
+++ LSJCurrencyCalculator/LSJCurrencyCalculator/Data/Coredata/CoreDataFavoriteRepository.swift
@@ -5,4 +5,55 @@
 //  Created by yimkeul on 4/16/25.
 //
 
+import CoreData
 import Foundation
+
+final class CoreDataFavoriteRepository: FavoriteRepositoryProtocol {
+    
+    private let persistentContainer: NSPersistentContainer
+    
+    init(persistentContainer: NSPersistentContainer) {
+        self.persistentContainer = persistentContainer
+    }
+
+    func fetchAllFavorites() throws -> [Favorite] {
+        let context = persistentContainer.viewContext
+        let request = Favorite.fetchRequest() // NSFetchRequest<Favorite>
+        return try context.fetch(request)
+    }
+
+    func addFavorite(currency: String, rate: Double) throws {
+        let context = persistentContainer.viewContext
+        let favorite = Favorite(context: context)
+        favorite.uuid = UUID()
+        favorite.currency = currency
+        favorite.rate = rate
+        favorite.isFavorite = true
+        try context.save()
+    }
+
+    func removeFavorite(currency: String) throws {
+        let context = persistentContainer.viewContext
+        let request = Favorite.fetchRequest()
+        request.predicate = NSPredicate(format: "currency == %@", currency)
+        let results = try context.fetch(request)
+        
+        for obj in results {
+            context.delete(obj)
+        }
+        try context.save()
+    }
+
+    func updateFavorite(currency: String, isFavorite: Bool) throws {
+        let context = persistentContainer.viewContext
+        let request = Favorite.fetchRequest()
+        request.predicate = NSPredicate(format: "currency == %@", currency)
+        let results = try context.fetch(request)
+        
+        if let target = results.first {
+            target.isFavorite = isFavorite
+            try context.save()
+        }
+    }
+}
+
