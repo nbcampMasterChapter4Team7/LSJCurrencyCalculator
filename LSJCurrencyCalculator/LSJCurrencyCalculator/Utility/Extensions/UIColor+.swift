599ffb0462fe3e4aefb97bcfe5b13ee06a0cd0f2
Yimkeul <leesjun29@gmail.com>
2025-04-17 15:20:39 +0900

    [Feat] #9 - UIColer Extension 추가

--- LSJCurrencyCalculator/LSJCurrencyCalculator/Utility/Extensions/UIColor+.swift
+++ LSJCurrencyCalculator/LSJCurrencyCalculator/Utility/Extensions/UIColor+.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIColor {
+    static func asset(_ asset: Asset) -> UIColor {
+        guard let color = UIColor(named: asset.rawValue) else {
+            fatalError("Color asset '\(asset.rawValue)' not found in Asset Catalog")
+        }
+        return color
+    }
+    enum Asset: String {
+        case backgroundColor = "BackgroundColor"
+        case textColor = "TextColor"
+        case secondaryTextColor = "SecondaryTextColor"
+        case cellBackgroundColor = "CellBackgroundColor"
+        case favoriteColor = "FavoriteColor"
+        case buttonColor = "ButtonColor"
+    }
+}
