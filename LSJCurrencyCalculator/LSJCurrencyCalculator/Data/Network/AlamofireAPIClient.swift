ff13d57315d2822c49a937270f9d5f522c4b4817
Yimkeul <leesjun29@gmail.com>
2025-04-21 17:12:24 +0900

    [Feat] #12 - Alamofire 추가

--- LSJCurrencyCalculator/LSJCurrencyCalculator/Data/Network/AlamofireAPIClient.swift
+++ LSJCurrencyCalculator/LSJCurrencyCalculator/Data/Network/AlamofireAPIClient.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import Alamofire
+
+/// Alamofire 기반의 네트워크 클라이언트
+final class AlamofireAPIClient: APIClientProtocol {
+    
+    /// 테스트 시점에 MockSession을 주입할 수 있도록, Session을 DI
+    private let session: Session
+    
+    init(session: Session = .default) {
+        self.session = session
+    }
+    
+    func request<T: Decodable>(
+        _ endpoint: Endpoint,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
+        session
+            .request(endpoint.urlRequest)        // URLRequest에서 HTTPMethod, headers도 가져옴
+            .validate()                         // 상태코드 200~299만 통과
+            .responseDecodable(of: T.self) { res in
+                switch res.result {
+                case .success(let value):
+                    completion(.success(value))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
